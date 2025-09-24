1656cf4e24657921bd3052090e1071202e8ac7e0
dipanshu528 <vermadipanshu528@gmail.com>
2025-09-24 21:59:17 +0530

    adding cors

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const connectToMongo = require("./db");
 const app = express();
-
+const cors = require("cors");
 
 const port = 4000;
 
-// const cors = require('cors');
+
 app.use(cors({
     origin: "https://mappin-project.netlify.app",
   methods: ["GET","POST", "DELETE"],  
   credentials: true
  }
  ));
-app.use(cors());
+// app.use(cors());
 
 
 connectToMongo();
